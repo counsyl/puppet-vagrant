5da47e3de4e7c1ee3931c09575417b16044c2c2d
Justin Bronn <justin@counsyl.com>
2013-12-19 23:42:33 -0800

    Upgrade to Vagrant 1.4.1.

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,8 +11,7 @@ class vagrant::params {
   }
 
   # The version of Vagrant to install, and the Git hash of the tagged version.
-  $version  = '1.3.5'
-  $version_hash = 'a40522f5fabccb9ddabad03d836e120ff5d14093'
+  $version = '1.4.1'
 
   # Where to cache Vagrant package downloads, if necessary.
   $cache = '/var/cache/vagrant'
@@ -41,9 +40,7 @@ class vagrant::params {
     }
   }
 
-  # Download URLs, unfortunately Vagrant static download files are tied
-  # to the git commit hash of the version tag.  This hash is necessary to
-  # construct the $base_url parameter.
-  $base_url = "http://files.vagrantup.com/packages/${version_hash}/"
+  # The download URL for Vagrant.
+  $base_url = 'https://dl.bintray.com/mitchellh/vagrant/'
   $package_url = "${base_url}${package_basename}"
 }
