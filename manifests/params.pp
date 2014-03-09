10453308781281dc62006a1d0661237b4be5d576
Justin Bronn <justin@counsyl.com>
2014-03-09 14:22:11 -0700

    Upgrade to Vagrant 1.4.3.

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,7 +11,7 @@ class vagrant::params {
   }
 
   # The version of Vagrant to install.
-  $version = '1.4.2'
+  $version = '1.4.3'
 
   # Where to cache Vagrant package downloads, if necessary.
   $cache = '/var/cache/vagrant'
