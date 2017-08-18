ef795cc1a5591c14785ada33d8c5f0c9b33f02ea
Taj Morton <taj@counsyl.com>
2017-08-18 09:05:42 -0700

    upgrade to installing Vagrant 1.9.7 by default

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,14 +4,14 @@
 #
 class vagrant::params {
   # Setting up properties for the package.
-  if $::architecture == 'amd64' {
+  if ($::architecture == 'amd64' or $::architecture == 'x86_64') {
     $arch = 'x86_64'
   } else {
     $arch = 'i686'
   }
 
   # The version of Vagrant to install.
-  $version = '1.7.4'
+  $version = hiera('vagrant::version', '1.7.4')
 
   # Where to cache Vagrant package downloads, if necessary.
   $cache = '/var/cache/vagrant'
@@ -19,7 +19,13 @@ class vagrant::params {
   case $::osfamily {
     'Darwin': {
       $package = "vagrant-${version}"
-      $package_basename = "vagrant_${version}.dmg"
+      if versioncmp($version, '1.9.2') > 0 {
+        # from version 1.9.3 upward, the .dmg includes the architecture
+        $package_basename = "vagrant_${version}_${arch}.dmg"
+      }
+      else {
+        $package_basename = "vagrant_${version}.dmg"
+      }
       $provider = 'pkgdmg'
       $download = false
     }
@@ -41,6 +47,7 @@ class vagrant::params {
   }
 
   # The download URL for Vagrant.
-  $base_url = 'https://dl.bintray.com/mitchellh/vagrant/'
-  $package_url = "${base_url}${package_basename}"
+  $base_url = hiera(
+      'vagrant::package_url', 'https://releases.hashicorp.com/vagrant/')
+  $package_url = "${base_url}${version}/${package_basename}"
 }
